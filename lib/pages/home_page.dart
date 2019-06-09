ee1d2854704f492baf9e9e46ffc06fb4440daf89
Akshay <akshayyadav7613@gmail.com>
2019-06-09 12:53:10 +0530

    formated the code

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,18 +4,32 @@ import 'package:learn_flutter_dart/products_manger.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-       drawer: Drawer(
-         child: Column(
-        children: <Widget>[
-          AppBar(title: Text("choose"), iconTheme: IconThemeData(color: Colors.white)),
-          ListTile(title: Text("manage products",style: TextStyle(fontSize: 17),),onTap: (){},)
-        ],
-       ),),
-          appBar: AppBar(title: Text("easylit",style: TextStyle(color: Colors.white),
-          ),iconTheme: IconThemeData(color: Colors.white),
+    return Scaffold(
+        drawer: Drawer(
+          child: Column(
+            children: <Widget>[
+              AppBar(
+                title: Text("choose"),
+                iconTheme: IconThemeData(color: Colors.white),
+                automaticallyImplyLeading: false,
+              ),
+              ListTile(
+                title: Text(
+                  "manage products",
+                  style: TextStyle(fontSize: 17),
+                ),
+                onTap: () {},
+              )
+            ],
           ),
-          body: ProductsManger());
-
+        ),
+        appBar: AppBar(
+          title: Text(
+            "easylit",
+            style: TextStyle(color: Colors.white),
+          ),
+          iconTheme: IconThemeData(color: Colors.white),
+        ),
+        body: ProductsManger());
   }
 }
