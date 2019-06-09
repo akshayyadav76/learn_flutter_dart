d05df574c351133ec10d87d0cc41921aa5f5f55d
Akshay <akshayyadav7613@gmail.com>
2019-06-09 20:47:35 +0530

    add two new page for tabs

--- lib/pages/product_list.dart
+++ lib/pages/product_list.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class CreateProductsPage extends StatelessWidget {
+class ListProductsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container();
