60ec5aef7f03da6b5f7250b53d060491f6d0d2f0
Akshay <akshayyadav7613@gmail.com>
2019-06-11 21:05:39 +0530

    finally end......^^)

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class CreateProductsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Create product"),);
+    return Center(child: RaisedButton(child: Text("save"),onPressed: (){
+      showModalBottomSheet(context: context,builder: (BuildContext context){
+        return Center(child: Text("see the produts here"),);
+      });
+    },));
   }
 }
