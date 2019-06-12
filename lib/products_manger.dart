31132ba7cc4ec5b8bd0886ee3762e8433d512e3d
Akshay <akshayyadav7613@gmail.com>
2019-06-12 21:01:00 +0530

    set the code...

--- lib/products_manger.dart
+++ lib/products_manger.dart
@@ -1,25 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:learn_flutter_dart/products.dart';
-import 'package:learn_flutter_dart/product_control.dart';
+
 
 class ProductsManger extends StatelessWidget {
   final List<Map<String,String>> products;
-  final Function addproduct;
-  final Function deleteproduct;
-  ProductsManger(this.products,this.addproduct,this.deleteproduct);
 
 
+  ProductsManger(this.products);
   @override
   Widget build(BuildContext context) {
     return  Column(
       children: [
-        Container(
-      margin: EdgeInsets.all(10.0),
-      child: ProductControl(addproduct),
-    ),
-       Expanded(child:  Products(products,delete: deleteproduct,)),
+       Expanded(child:  Products(products)),
     ],
-
     );
   }
 }
