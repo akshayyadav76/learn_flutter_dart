8ee73834778f661e0bba591d3003926e565ca76c
Akshay <akshayyadav7613@gmail.com>
2019-06-13 20:27:28 +0530

    add as home route

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,21 +1,62 @@
 import 'package:flutter/material.dart';
 
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
 
-class Login extends StatelessWidget {
+class _LoginState extends State<Login> {
+  String _id;
+  String _pass;
+  bool _accept = false;
+
   @override
   Widget build(BuildContext context) {
-    return   Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: Text("Login"),
       ),
-      body: Center(
-        child: RaisedButton(
-            child: Text("Login"),
-            onPressed: () {
-
-             Navigator.pushReplacementNamed(context, '/');
-            }),
-      ),);
-
+      body: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 20,
+          ),
+          TextField(
+            autofocus: true,
+            decoration: InputDecoration(labelText: "E-mail"),
+            onChanged: (String value) {
+              setState(() {
+                _id =value;
+              });
+            },
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          TextField(
+            autofocus: true,
+            decoration: InputDecoration(labelText: "password"),
+            onChanged: (String value) {
+              setState(() {
+                _pass = value;
+              });
+            },
+          ),
+          SwitchListTile(
+              value: _accept,
+              onChanged: (bool value) {
+                setState(() {
+                  _accept = value;
+                });
+              },
+              title: Text("accept terms & conditions")),
+          RaisedButton(
+              child: Text("Login"),
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, 'second');
+              }),
+        ],
+      ),
+    );
   }
 }
