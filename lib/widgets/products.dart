2313aca493e893f782345a066d0afd5936d0e99d
Akshay <akshayyadav7613@gmail.com>
2019-06-17 22:22:53 +0530

    seperate card

--- lib/widgets/products.dart
+++ lib/widgets/products.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../pages/deatils.dart';
 import './price_tag.dart';
+import 'produts_card.dart';
 
 
 class Products extends StatelessWidget {
@@ -9,56 +10,16 @@ class Products extends StatelessWidget {
 
   Products(this.products);
 
-  Widget seeprodcts(BuildContext context, int index) {
-    return Card(
-      child: Column(
-        children: <Widget>[
-          Image.asset(products[index]['image']),
-          Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,children: <Widget>[
-           // SizedBox(width: 80,),
-            Text(products[index]['title'],style: TextStyle(fontStyle: FontStyle.italic,fontSize: 20),),
-            //SizedBox(width: 40,),
-
-            Price_tag(products[index]['price'].toString()),
-
-          ],),
-          SizedBox(height: 10,),
-          Container(
-            decoration: BoxDecoration(border: Border.all(color: Colors.deepPurple)),
-            child: Text('Lalghati,Airport road,Bhopal',
-              style: TextStyle(fontStyle: FontStyle.italic,fontSize: 20),),
-          ),
-
-          ButtonBar(
-            alignment: MainAxisAlignment.center,
-            children: <Widget>[
-              IconButton(
-                icon: Icon(Icons.assignment_late),
-                  onPressed: () {
-                    Navigator.pushNamed<bool>(context,'/products/'+index.toString());
-
-                  }
-                  ),
-              IconButton(
-                  icon: Icon(Icons.favorite_border),
-                  color: Colors.red,
-                  onPressed: () {
-                    Navigator.pushNamed<bool>(context,'/products/'+index.toString());
-
-                  }
-              )
-            ],
-          )
-        ],
-      ),
-    );
-  }
+//  Widget seeprodcts(BuildContext context, int index) {
+//    return
+//  }
 
   Widget buildproductlist() {
     Widget ProdcutCard;
     if (products.length > 0) {
       ProdcutCard = ListView.builder(
-        itemBuilder: seeprodcts,
+        itemBuilder: (BuildContext context,int index)=> ProductsCard(products[index], index),
+
         itemCount: products.length,
       );
     } else {
---
+++ lib/widgets/produts_card.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'price_tag.dart';
+
+class ProductsCard extends StatelessWidget {
+  final Map<String ,dynamic>product;
+  final int productindex;
+
+  ProductsCard(this.product, this.productindex);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image.asset(product['image']),
+          Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,children: <Widget>[
+            // SizedBox(width: 80,),
+            Text(product['title'],style: TextStyle(fontStyle: FontStyle.italic,fontSize: 20),),
+            //SizedBox(width: 40,),
+
+            Price_tag(product['price'].toString()),
+
+          ],),
+          SizedBox(height: 10,),
+          Container(
+            decoration: BoxDecoration(border: Border.all(color: Colors.deepPurple)),
+            child: Text('Lalghati,Airport road,Bhopal',
+              style: TextStyle(fontStyle: FontStyle.italic,fontSize: 20),),
+          ),
+
+          ButtonBar(
+            alignment: MainAxisAlignment.center,
+            children: <Widget>[
+              IconButton(
+                  icon: Icon(Icons.assignment_late),
+                  onPressed: () {
+                    Navigator.pushNamed<bool>(context,'/products/'+productindex.toString());
+
+                  }
+              ),
+              IconButton(
+                  icon: Icon(Icons.favorite_border),
+                  color: Colors.red,
+                  onPressed: () {
+                    Navigator.pushNamed<bool>(context,'/products/'+productindex.toString());
+
+                  }
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
