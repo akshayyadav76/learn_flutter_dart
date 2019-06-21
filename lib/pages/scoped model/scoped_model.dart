4b6fd18262513c48aaa412392385be168159f3d3
Akshay <akshayyadav7613@gmail.com>
2019-06-21 20:14:52 +0530

    now apply scped model to the app

---
+++ lib/pages/scoped model/scoped_model.dart
@@ -0,0 +1,27 @@
+import 'package:scoped_model/scoped_model.dart';
+import 'package:learn_flutter_dart/module/products_map.dart';
+
+
+class ScopedModel extends Model{
+
+  List<ProductsMap> _products = [];
+  List<ProductsMap> get products{
+    return List.from(_products);
+  }
+
+  void addproduct(ProductsMap productsdata) {
+
+      _products.add(productsdata);
+
+  }
+
+  void deleteprodcut(int index) {
+    _products.removeAt(index);
+  }
+
+  void update(int index, ProductsMap product) {
+
+      _products[index] = product;
+
+  }
+}
